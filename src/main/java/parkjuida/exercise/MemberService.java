17ac719403a6721f698a1a973ea6aff687ab1b68
parkjuida <bees1114@naver.com>
2021-03-21 22:21:49 +0900

    Add member repository and service

---
+++ src/main/java/parkjuida/exercise/MemberService.java
@@ -0,0 +1,39 @@
+package parkjuida.exercise;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import parkjuida.exercise.domain.Member;
+import parkjuida.exercise.repository.MemberRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("Already exists.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
