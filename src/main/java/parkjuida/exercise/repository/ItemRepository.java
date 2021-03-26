64e187be404c6d5b626373115c7086ce6fbd1c0d
parkjuida <bees1114@naver.com>
2021-03-26 21:19:51 +0900

    Add item domain

---
+++ src/main/java/parkjuida/exercise/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package parkjuida.exercise.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import parkjuida.exercise.domain.item.Item;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    @PersistenceContext
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
